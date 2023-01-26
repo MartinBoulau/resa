8842c81720e23513856244fd193ff09fe7176d13
SwaiiZ <le.bail.bastienlb@gmail.com>
2023-01-26 09:50:20 +0100

    [FIX] - .gitignore

--- launch.sh
+++ launch.sh
@@ -5,3 +5,6 @@ docker exec -it symfony-php composer install
 docker exec -it symfony-php composer update
 docker exec -it symfony-api composer install
 docker exec -it symfony-api composer update
+
+# Import data into database
+docker exec -it mysql -u username -p database_name < file.sql
